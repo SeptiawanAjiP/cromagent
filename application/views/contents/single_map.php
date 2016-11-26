c742d071a3fa76dae6820a58742c3151d15463ec
CholidAlFarisi <muchammadcholidalfarisi@gmail.com>
2016-11-26 17:39:48 +0700

    Add files via upload

---
+++ application/views/contents/single_map.php
@@ -0,0 +1,21 @@
+<!--main content start-->
+    <!-- page start-->
+        <div class="row">
+            <div class="col-sm-12">
+                <section class="panel">
+                    <header class="panel-heading">
+                        Shop Location
+                        <span class="tools pull-right">
+                            <a href="javascript:;" class="fa fa-chevron-down"></a>
+                            <a href="javascript:;" class="fa fa-cog"></a>
+                            <a href="javascript:;" class="fa fa-times"></a>
+                         </span>
+                    </header>
+                    <div class="panel-body">
+                        <div id="gmap-list"></div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    <!--page end-->
+<!--main content end-->
